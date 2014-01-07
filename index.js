3897480a70eb70b1e15d489d62d6931cb1efee3e
Sebastien Ballesteros <sebastien.ballesteros@gmail.com>
2014-01-07 14:42:40 -0500

    validation functions for cohesive semver versioning

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+//NOTE: this module has to be written so that it can be used from within CouchDb...
+
+var isUrl = require('is-url')
+  , semver = require('semver')
+  , url = require('url');
+
+var BASE = "https://registry.standardanalytics.io/";
+var URL = 'https://registry.standardanalytics.io/contexts/datapackage.jsonld';
+
+exports.contextUrl = URL;
+exports.link = '<' + URL + '>; rel="http://www.w3.org/ns/json-ld#context"; type="application/ld+json"';
+
 exports.context = {
   "@context": {
-    "@base": "https://registry.standardanalytics.io/",
+    "@base": BASE,
 
     "spec": "http://schema.standardanalytics.io/spec/",
     "sch":  "http://schema.org/",
@@ -56,10 +68,95 @@ exports.context = {
   }
 };
 
-var URL = 'https://registry.standardanalytics.io/contexts/datapackage.jsonld';
+//!! only check stuff that will be accessed within the registry.
+exports.schema = {
+  $schema: 'http://json-schema.org/draft-04/schema#',
+  type: 'object', 
+  properties: {
+    name: { type: 'string' },
+    version: { type: 'string' },
+    description: { type: 'string' },
+    keywords: { type: 'array', items: { type: 'string' } },
+    isBasedOnUrl: { type: 'array', items: { type: 'string' } },
+    dataset: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          name: { type: 'string'},
+          description: { type: 'string'},
+          url:  { type: 'string' }, 
+          path: { type: 'string'},
+          encoding: {
+            type: 'object',
+            properties: {
+              encodingFormat: { type: 'string' },
+              contentSize: { type: 'integer' }
+            }
+          }
+        },
+        required: [ 'name' ]
+      }
+    },
+    analytics: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          name: { type: 'string'},
+          description: { type: 'string'},
+          programmingLanguage: {
+            type: 'object',
+            properties: {
+              name: { type: 'string' },
+            },
+            required: [ 'name' ]
+          },
+          runtime: { type: 'string' },
+          targetProduct: { 
+            type: 'object',
+            properties: {
+              operatingSystem: { type: 'string' }
+            },
+            required: [ 'operatingSystem' ]
+          },
+          sampleType: { type: 'string'},
+          codeRepository: { type: 'string'},
+          input:  { type: 'array', items: { type: 'string'} },
+          output: { type: 'array', items: { type: 'string'} }
+        }
+      }
+    }
+  },
+  required: ['name', 'version']
+};
 
-exports.contextUrl = URL;
-exports.link = '<' + URL + '>; rel="http://www.w3.org/ns/json-ld#context"; type="application/ld+json"';
+
+/**
+ * add @type to an obj if possible
+ */
+function _addType(x, type){
+
+  if( (typeof x !== 'object') || Array.isArray(x) ){
+    return;
+  }
+  
+  //x is an obj
+  if( !('@type' in x) ){
+    x['@type'] = type;
+  } else if(Array.isArray(x['@type'])){
+    if(x['@type'].indexOf(type) === -1){
+      x['@type'].push(type);
+    }
+  } else if(typeof x['@type'] === 'string'){
+    if(x['@type'] !== type){
+      x['@type'] = [x['@type'], type];
+    }
+  }
+
+  return x;
+};
+
 
 /**
  * modifies dpkg in place to add @id, @type and optionaly @context
@@ -79,29 +176,27 @@ exports.ify = function(dpkg, options){
   dpkg['@type'] = 'DataCatalog';
 
   if( 'author' in dpkg && !('@type' in dpkg.author) ){ //pre-existing type might be Organization
-    dpkg.author['@type'] = 'Person'; 
+    _addType(dpkg.author, 'Person');
   }
 
   if('repository' in dpkg){
     dpkg.repository.forEach(function(c){
-      c['@type'] = 'Code';
+      _addType(c, 'Code');
     });
   }
 
   if('contributor' in dpkg){
     dpkg.contributor.forEach(function(c){
       if ( !('@type' in c) ) { //pre-existing type might be Organization
-        c['@type'] = 'Person';
+        _addType(c, 'Person');
       }
     });
   }
 
   if('analytics' in dpkg){
     dpkg.analytics.forEach(function(r){
-      r['@type'] = 'Code';
-      if('programmingLanguage' in r){
-        r.programmingLanguage['@type'] = 'SoftwareApplication'
-      }
+      _addType(r, 'Code');
+      _addType(r.programmingLanguage, 'SoftwareApplication');
     });
   }
 
@@ -111,27 +206,120 @@ exports.ify = function(dpkg, options){
         r['@id'] = dpkg['@id'] + '/' + r.name;
       }
 
-      if('@type' in r){
-        if(Array.isArray(r['@type'])){
-          if(r['@type'].indexOf('DataSet') === -1){
-            r['@type'].push('DataSet');
-          }
-        } else if (r['@type'] !== 'DataSet') {
-          r['@type'] = [ r['@type'], 'DataSet' ];
-        }
-      } else {
-        r['@type'] = 'DataSet';
-      }
+      _addType(r, 'DataSet');
+      _addType(r.encoding, 'DataDownload');
+      _addType(r.distribution, 'DataDownload');
 
-      if('distribution' in r){
-        r.distribution['@type'] = 'DataDownload';
-      }
-
-      r.catalog = { name: dpkg.name, version: dpkg.version, url: dpkg['@id'] }
+      r.catalog = { name: dpkg.name, version: dpkg.version, url: dpkg['@id'] };
     });
   }
 
   dpkg.catalog = { name: dpkg.name, url: dpkg.name };
 
   return dpkg;
+};
+
+
+/**
+ * validate isBasedOnUrl and returns a dataDependencies hash
+ */
+exports.dataDependencies = function(isBasedOnUrl){
+
+  if(!isBasedOnUrl) return undefined;
+
+  var dataDependencies = {};
+
+  isBasedOnUrl.forEach(function(uri){
+    var absUrl = (isUrl(uri)) ? uri : url.resolve(BASE, uri);
+    var urlObj = url.parse(absUrl, true);
+
+    if(urlObj.hostname === url.parse(BASE).hostname){ //it's a dpkg of this registry
+      var pathname = urlObj.pathname.replace(/^\//, '');
+      var splt = pathname.split('/'); //name, version, ...
+      if(splt.length < 2){
+        throw new Error('invalid URI: '+ uri);
+      }
+
+      var name = splt[0];
+      var version = splt[1];
+      if(version === 'latest'){
+        version = (urlObj.query && urlObj.query.range) || '*';
+      }
+
+      if(!semver.validRange(version)){
+        throw new Error('invalid version/range '+ version);        
+      }
+
+      if(name in dataDependencies){
+        throw new Error(name + ' is already listed as dependencies');
+      }
+      dataDependencies[name] = version;
+    }
+  });
+
+  return dataDependencies;
+};
+
+/**
+ * make sure that link to dpkg hosted on the registry respect the
+ * versioning scheme used
+ */
+function validateRequiredUri(uri, name, version, dataDependencies){
+
+  var dataDependencies = dataDependencies || {};
+
+  var absUrl = (isUrl(uri)) ? uri : url.resolve(BASE, uri);
+  var urlObj = url.parse(absUrl);
+
+  if(urlObj.hostname === url.parse(BASE).hostname){ //it's a dpkg of this registry
+    var pathname = urlObj.pathname.replace(/^\//, '');
+    var splt = pathname.split('/'); //name, version, ...
+
+    if(splt.length < 2){
+      throw new Error('invalid URI: '+ uri);
+    }
+    
+    if (splt[0] === name){ //whithin dpkg link
+      if (splt[1] !== version){
+        throw new Error('version mismatch for :' + uri);
+      } else {
+        return;
+      };
+    } else { //link to another dpkg on this registry: does it satisfies the data dependencies constraints ?
+
+      if(!(splt[0] in dataDependencies)){
+        throw new Error( splt[0] + '/' + splt[1] + ' is not listed in isBasedOnUrl (required in ' + uri  + ')');
+      } else {        
+        //check version match
+        if(!semver.satisfies(splt[1], dataDependencies[splt[0]])){
+          throw new Error( uri + ' does not respect semver requirement of isBasedOnUrl (' + dataDependencies[splt[0]] + ')');
+        };
+      }      
+    }
+  }
+};
+exports.validateRequiredUri = validateRequiredUri;
+
+exports.validateRequire = function(dpkg, dataDependencies){
+
+  var dataDependencies = dataDependencies || exports.dataDependencies(dpkg.isBasedOnUrl);
+
+  var dataset = dpkg.dataset || [];
+  dataset.forEach(function(r){
+    if('url' in r) {
+      validateRequiredUri(r.url, dpkg.name, dpkg.version, dataDependencies);
+    }
+  });
+
+  var analytics = dpkg.analytics || [];
+  analytics.forEach(function(r){
+    ['input', 'output'].forEach(function(prop){
+      if (prop in r) {
+        r[prop].forEach(function(uri){
+          validateRequiredUri(uri, dpkg.name, dpkg.version, dataDependencies);
+        });
+      }
+    });
+  });
+
 };
--- test/test.js
+++ test/test.js
@@ -1,47 +1,134 @@
 var fs = require('fs')
+  , url = require('url')
   , assert = require('assert')
   , clone = require('clone')
-  , ldjson = require('..')
+  , dpkgJsonLd = require('..')
   , path = require('path');
 
 
 var root = path.dirname(__filename);
+var base = dpkgJsonLd.context['@context']['@base'];
 
 var dpkg = require(path.join(root, 'fixture', 'package.json'));
 
 describe('datapacakge-jsonld', function(){
 
-  it('should jsonldify', function(){
-    
-    var ldpkg = ldjson.ify(clone(dpkg));
+  describe('jsonldify', function(){
 
-    assert.equal(ldpkg['@context'], ldjson.contextUrl);
-    assert.equal(ldpkg['@id'], 'mydpkg/0.0.0');
-    assert.equal(ldpkg['@type'], 'DataCatalog');
-    assert.equal(ldpkg.author['@type'], 'Person');
-    assert.equal(ldpkg.repository[0]['@type'], 'Code');
-    assert.deepEqual(ldpkg.catalog, { name: 'mydpkg', url: 'mydpkg' } );
-    ldpkg.dataset.forEach(function(r){
-      assert.equal(r['@type'], 'DataSet');
-      assert.equal(r['@id'], 'mydpkg/0.0.0/' + r.name);
-      assert.deepEqual(r.catalog, { name: 'mydpkg', version: '0.0.0', url: 'mydpkg/0.0.0' } );
-      assert.equal(r.distribution['@type'], 'DataDownload');
+    it('should add @type and @id', function(){
+      
+      var ldpkg = dpkgJsonLd.ify(clone(dpkg));
+
+      assert.equal(ldpkg['@context'], dpkgJsonLd.contextUrl);
+      assert.equal(ldpkg['@id'], 'mydpkg/0.0.0');
+      assert.equal(ldpkg['@type'], 'DataCatalog');
+      assert.equal(ldpkg.author['@type'], 'Person');
+      assert.equal(ldpkg.repository[0]['@type'], 'Code');
+      assert.deepEqual(ldpkg.catalog, { name: 'mydpkg', url: 'mydpkg' } );
+      ldpkg.dataset.forEach(function(r){
+        assert.equal(r['@type'], 'DataSet');
+        assert.equal(r['@id'], 'mydpkg/0.0.0/' + r.name);
+        assert.deepEqual(r.catalog, { name: 'mydpkg', version: '0.0.0', url: 'mydpkg/0.0.0' } );
+        assert.equal(r.distribution['@type'], 'DataDownload');
+      });
+
+    });
+
+    it('should respect pre-existing dataset @type', function(){
+      var mydpkg = clone(dpkg);
+      mydpkg.dataset[0]['@type'] = 'StatTest';
+      var ldpkg = dpkgJsonLd.ify(mydpkg);
+      assert.deepEqual(['StatTest', 'DataSet'], ldpkg.dataset[0]['@type']);
+    });
+
+    it('should respect pre-existing dataset @type array', function(){
+      var mydpkg = clone(dpkg);
+      mydpkg.dataset[0]['@type'] = ['StatTest', 'Chi2Test'];
+      var ldpkg = dpkgJsonLd.ify(mydpkg);
+      assert.deepEqual(['StatTest', 'Chi2Test', 'DataSet'], ldpkg.dataset[0]['@type']);
     });
 
   });
 
-  it('should respect pre-existing dataset @type', function(){
-    var mydpkg = clone(dpkg);
-    mydpkg.dataset[0]['@type'] = 'StatTest';
-    var ldpkg = ldjson.ify(mydpkg);
-    assert.deepEqual(['StatTest', 'DataSet'], ldpkg.dataset[0]['@type']);
+  describe('dataDependencies', function(){
+    
+    it('should extract dataDependencies', function(){
+      var isBasedOnUrl = [
+        'dpkg0/0.0.0',
+        'dpkg1/latest',
+        url.resolve(base, 'dpkg2/0.0.0'),
+        url.resolve(base, 'dpkg3/0.0.1'),
+        url.resolve(base, 'dpkg4/latest'),
+        url.resolve(base, 'dpkg5/latest?range=' + encodeURIComponent('>2.0.0'))
+      ];
+      var dataDependencies = dpkgJsonLd.dataDependencies(isBasedOnUrl);
+      var expected = { dpkg0: '0.0.0', dpkg1: '*', dpkg2: '0.0.0', dpkg3: '0.0.1', dpkg4: '*', dpkg5: '>2.0.0' };
+
+      assert.deepEqual(dataDependencies, expected);
+    });
+
+    it('should throw an error when a version range does not respect semver', function(){      
+      assert.throws( function(){ dpkgJsonLd.dataDependencies(['dpkg/a.b.c']); }, Error );
+    });
+    
+    it('should throw an error when a dpkg as an invalid URI', function(){
+      assert.throws( function(){ dpkgJsonLd.dataDependencies(['dpkg']); }, Error);
+      assert.throws( function(){ dpkgJsonLd.dataDependencies(['']); }, Error );
+      assert.throws( function(){ dpkgJsonLd.dataDependencies([base]); }, Error );            
+    });
+
+    it('should throw an error when a dpkg is listed many times as dep', function(){      
+      assert.throws( function(){ dpkgJsonLd.dataDependencies(['dpkg/0.0.0', url.resolve(base, 'dpkg/0.1.1')]); }, Error );     
+    });
+
   });
 
-  it('should respect pre-existing dataset @type array', function(){
-    var mydpkg = clone(dpkg);
-    mydpkg.dataset[0]['@type'] = ['StatTest', 'Chi2Test'];
-    var ldpkg = ldjson.ify(mydpkg);
-    assert.deepEqual(['StatTest', 'Chi2Test', 'DataSet'], ldpkg.dataset[0]['@type']);
+  describe('required links', function(){
+
+    it('should validate a valid required uri', function(){
+      assert(!dpkgJsonLd.validateRequiredUri('dpkg2/0.0.0', 'dpkg', '0.0.0', {dpkg2: '*'}));
+    });
+    
+    it('should throw an error when on whithin dpkg version mismatch', function(){
+      assert.throws( function(){ 
+        dpkgJsonLd.validateRequiredUri('dpkg/0.0.0', 'dpkg', '0.0.1', {});
+      }, Error);
+    });
+
+    it('should throw an error when a required uri is not listed as dep', function(){
+      assert.throws( function(){ 
+        dpkgJsonLd.validateRequiredUri('dpkg2/0.0.0', 'dpkg', '0.0.0', {});
+      }, Error);
+    });
+
+    it('should throw an error when a required uri does not satisfy the dep version', function(){
+      assert.throws( function(){ 
+        dpkgJsonLd.validateRequiredUri('dpkg2/0.0.0', 'dpkg', '0.0.0', {'dpkg2':'0.1.0'});
+      }, Error);
+    });
+
+    it('should throw an error on invalid uri', function(){
+      assert.throws( function(){ 
+        dpkgJsonLd.validateRequiredUri('dpkg2', 'dpkg', '0.0.0', {'dpkg2':'0.1.0'});
+      }, Error);
+    });
+
+  });
+
+
+  describe('validate require', function(){
+    it('should validate when a package has valid require link', function(){
+      assert(!dpkgJsonLd.validateRequire(dpkg));
+    });
+
+    it('should throw an error when a package has invalid require links', function(){
+      assert.throws( function(){ 
+        var mydpkg = clone(dpkg);
+        mydpkg.analytics = [ { input: ['dpkg5/0.0.0'] } ];
+        dpkgJsonLd.validateRequire(mydpkg)
+      }, Error);
+    });
+
   });
 
 });
