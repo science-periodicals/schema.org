4cee10067db045c3d165f5f959001fe6657f3f3b
Sebastien Ballesteros <sebastien.ballesteros@gmail.com>
2016-01-30 21:22:01 -0500

    cleaning up

--- data/index.js
+++ data/index.js
@@ -2,36 +2,93 @@
  * convert schema.org raw RDFa data into compacted JSON-LD
  */
 
-var fs = require('fs')
-  , saTerms = require('../lib/terms')
-  , request = require('request')
-  , jsonld = require('jsonld')();
+var fs = require('fs');
+var jsonldRdfaParser = require('jsonld-rdfa-parser');
+var jsonld = require('jsonld');
+var util = require('util');
 
-jsonld.use('request'); //needed to parse RDFa
+// register the parser for content type text/html
+jsonld.registerRDFParser('text/html', jsonldRdfaParser);
 
-//get context from SA ontologie
-var ctx = saTerms()['@context'];
+var context = {
+  "@context": {
+    "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+    "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
+    "owl": "http://www.w3.org/2002/07/owl#",
+    "vs": "http://www.w3.org/2003/06/sw-vocab-status/ns#",
+    "xsd": "http://www.w3.org/2001/XMLSchema#",
+    "bibo": "http://purl.org/ontology/bibo/",
+    "skos": "http://www.w3.org/2004/02/skos/core#",
+    "dc": "http://purl.org/dc/terms/",
+    "schema": "http://schema.org/",
+    "oa": "http://www.w3.org/ns/oa#",
 
-//patch for schema.org specific conventions
-ctx["domainIncludes"] = {
-  "@id": "schema:domainIncludes",
-  "@type": "@id"
+    "sameAs": {
+      "@id": "owl:sameAs",
+      "@type": "@id"
+    },
+    "seeAlso": {
+      "@id": "rdfs:seeAlso",
+      "@type": "@id"
+    },
+    "equivalentClass": {
+      "@id": "owl:equivalentClass",
+      "@type": "@id"
+    },
+    "equivalentProperty": {
+      "@id": "owl:equivalentProperty",
+      "@type": "@id"
+    },
+
+    // remap custom schema.org terms to RDFS
+    "domain": {
+      "@id": "schema:domainIncludes",
+      "@type": "@id",
+      "@container": "@set"
+    },
+    "range": {
+      "@id": "schema:rangeIncludes",
+      "@type": "@id",
+      "@container": "@set"
+    },
+
+    "subClassOf": {
+      "@id": "rdfs:subClassOf",
+      "@type": "@id",
+      "@container": "@set"
+    },
+    "disjointWith": {
+      "@id": "owl:disjointWith",
+      "@type": "@id",
+      "@container": "@set"
+    },
+    "unionOf": {
+      "@id": "owl:unionOf",
+      "@type": "@id",
+      "@container": "@set"
+    },
+    "comment": "rdfs:comment",
+    "label": "rdfs:label",
+    "altLabel": "skos:altLabel",
+    "status": "vs:term_status",
+    "subPropertyOf": {
+      "@id": "rdfs:subPropertyOf",
+      "@type": "@id"
+    },
+    "source": {
+      "@id": "dc:source",
+      "@type": "@id"
+    },
+    "defines": { "@reverse": "rdfs:isDefinedBy" }
+  }
 };
 
-ctx["rangeIncludes"] = {
-  "@id": "schema:rangeIncludes",
-  "@type": "@id"
-};
-
-//jsonld.request will convert RDFa into a graph
-jsonld.request("http://schema.org/docs/schema_org_rdfa.html", function(err, res, dataRdfa) {
-  jsonld.compact(dataRdfa, ctx, function(err, dataJsonld) {
-    fs.writeFileSync('schema_org.jsonld', JSON.stringify(dataJsonld, null, 2));
+jsonld.fromRDF('http://schema.org/docs/schema_org_rdfa.html', {format: 'text/html'}, function(err, data) {
+  if (err) return console.error(err);
+  jsonld.compact(data, context, function(err, data) {
+    if (err) return console.error(err);
+    fs.writeFile('schema_org.jsonld', JSON.stringify(data, null, 2), function(err) {
+      if (err) console.error(err);
+    });
   });
-});
-
-
-//cache schema.org context
-request({url:'http://schema.org', headers: {'Accept': 'application/ld+json'}}, function(err, resp, body){
-  fs.writeFileSync('schema_org_context.jsonld', body);
 });
--- test/test.js
+++ test/test.js
@@ -1,149 +1,67 @@
-var fs = require('fs')
-  , path = require('path')
-  , url = require('url')
-  , assert = require('assert')
-  , clone = require('clone')
-  , jsonld = require('jsonld')
-  , SchemaOrgIo = require('..');
+import assert from 'assert';
+import SchemaOrg from '../src';
+import util from 'util';
 
-var root = path.dirname(__filename);
-
-describe('schema-org-io', function(){
-
-  describe('jsonld context', function(){
-    it('should standardize a JSONLD doc with the vanilla context', function(done){
-      var doc = {
-        "@context": SchemaOrgIo.context(),
-        "@id": "myNameSpace",
-        encoding: {contentUrl: 'http://example.com/data'},
-        distribution: {name: "name"},
-        hasPart: {'@id': 'http://example.com/part'}
-      };
-
-      jsonld.compact(doc, doc['@context'], function(err, cdoc){
-        assert.equal(cdoc['@id'], 'ldr:myNameSpace');
-        assert.deepEqual(cdoc.encoding, [doc.encoding]);
-        assert.deepEqual(cdoc.distribution, [doc.distribution]);
-        assert.deepEqual(cdoc.hasPart, [doc.hasPart['@id']]);
-        done();
-      });
-    });
+describe('schema-org', function() {
+  var schemaOrg;
+  before(function(){
+    schemaOrg = new SchemaOrg();
   });
 
-  describe('SchemaOrgIo', function(){
-    var packager;
-    before(function(){
-      packager = new SchemaOrgIo();
-    });
+  it('should have well initialized this.propMap', function() {
+    let map = schemaOrg.propMap['schema:valueReference'];
+    assert.deepEqual(Array.from(map.domain).sort(), [
+      'schema:PropertyValue',
+      'schema:QualitativeValue',
+      'schema:QuantitativeValue'
+    ].sort());
+    assert.deepEqual(Array.from(map.range).sort(), [
+      'schema:Enumeration',
+      'schema:PropertyValue',
+      'schema:QualitativeValue',
+      'schema:QuantitativeValue',
+      'schema:StructuredValue'
+    ].sort());
+  });
 
-    it('should have well initialized this.propMap', function(){
-      assert.deepEqual(packager.propMap['valueReference'], {
-        domains: [ 'QualitativeValue', 'QuantitativeValue' ],
-        ranges: [ 'Enumeration', 'StructuredValue' ]
-      });
-    });
+  it('should have well initialized this.classMap', function() {
+    let map = schemaOrg.classMap['schema:MedicalScholarlyArticle'];
+    assert.deepEqual(Array.from(map.subClassOf.keys()), [ 'schema:ScholarlyArticle' ]);
+    assert.deepEqual(Array.from(map.subClassOfChain.keys()), [ 'schema:ScholarlyArticle', 'schema:Article', 'schema:CreativeWork', 'schema:Thing' ]);
+  });
 
-    it('should have well initialized this.propMap with extra :ldrterms', function(){
-      assert.deepEqual(packager.propMap['hasPart'], {
-        domains: [ 'CreativeWork' ],
-        ranges: [ 'CreativeWork' ]
-      });
-    });
+  it('should return the subclasses of a className', function() {
+    assert.deepEqual(Array.from(schemaOrg.getSubClasses('schema:Article').keys()), [
+      'schema:APIReference',
+      'schema:BlogPosting',
+      'schema:DiscussionForumPosting',
+      'schema:LiveBlogPosting',
+      'schema:MedicalScholarlyArticle',
+      'schema:NewsArticle',
+      'schema:Report',
+      'schema:ScholarlyArticle',
+      'schema:SocialMediaPosting',
+      'schema:TechArticle'
+    ]);
+  });
 
-    it('should have well initialized this.classMap', function(){
-      assert.deepEqual(packager.classMap['MedicalScholarlyArticle'], {
-        subClasses: [ 'ScholarlyArticle' ],
-        subClassesChain: [ 'ScholarlyArticle', 'Article', 'CreativeWork', 'Thing' ]
-      });
-    });
+  it('should assess if a type is of a given class or not taking into account all the parent classes', function() {
+    assert(schemaOrg.is('schema:MedicalScholarlyArticle', 'schema:Article'));
+    assert(!schemaOrg.is('schema:MedicalScholarlyArticle', 'schema:QuantitativeValue'));
+  });
 
-    it.only('should return the subclasses of a className', function(){
-      assert.deepEqual(packager.getSubClasses('Article'), [ 'APIReference', 'BlogPosting', 'MedicalScholarlyArticle', 'NewsArticle', 'ScholarlyArticle', 'TechArticle' ]);
-    });
+  it('should assess if a class is more specific than another', function() {
+    assert(schemaOrg.isMoreSpecific('schema:MedicalScholarlyArticle', 'schema:Article'));
+    assert(!schemaOrg.isMoreSpecific('schema:Article', 'schema:MedicalScholarlyArticle'));
+  });
 
-    it('should throw an error for invalid @id', function(){
-      ['nobase', '.a', 'wrongprefix:a', 'ldr:/../', '../', 'ldr:/ns@version', 'ldr:cw%40version'].forEach(function(invalidId){
-        assert.throws( function(){ packager.validateId(invalidId); }, Error );
-      });
-    });
-
-    it('should throw an error for an invalid namespace @id', function(){
-      ['ldr:cw/nons', 'https://dcat.io/cw/nons'].forEach(function(invalidId){
-        assert.throws( function(){ packager.validateId(invalidId, {isNameSpace: true}); }, Error );
-      });
-    });
-
-    it('should validate @id and return a normalized version', function(){
-      assert.equal(packager.validateId('https://dcat.io/cw'), 'ldr:cw');
-      assert.equal(packager.validateId('ldr:cw'), 'ldr:cw');
-      assert.equal(packager.validateId('https://dcat.io/cw/a'), 'ldr:cw/a');
-      assert.equal(packager.validateId('ldr:cw/a'), 'ldr:cw/a');
-    });
-
-    it('should add @id', function(){
-      var doc = {
-        "@context": "https://dcat.io",
-        "@id": "ldr:cw",
-        "version": "0.0.0",
-        "name": 'myname',
-        "author": { "name": "peter" },
-        "encoding": { "name": "enc" },
-        "hasPart": [
-          { "@id": "ldr:cw/n1",  "name": "part a" },
-          { "name": "part b" },
-          { "name": "part c" }
-        ]
-      };
-
-      packager.setIds(doc);
-
-      assert.equal(doc['@id'], 'ldr:cw');
-      assert.equal(doc.author['@id'], 'ldr:cw/n0');
-      assert.equal(doc.encoding['@id'], 'ldr:cw/n2');
-      assert.equal(doc.hasPart[0]['@id'], 'ldr:cw/n1');
-      assert.equal(doc.hasPart[1]['@id'], 'ldr:cw/n3');
-      assert.equal(doc.hasPart[2]['@id'], 'ldr:cw/n4');
-    });
-
-    it('should add @id and respect options', function(){
-      var doc = {
-        "@context": "https://dcat.io",
-        "@id": "ldr:cw",
-        "version": "0.0.0",
-        "author": { "name": "peter" },
-        "encoding": { "name": "enc" },
-      };
-
-      var mdoc = packager.setIds(clone(doc), {ignoredProps: ['encoding'], restrictToClasses: ['CreativeWork']});
-      assert.deepEqual(doc, mdoc);
-    });
-
-    it('should infer type of a node', function(){
-      var obj = {
-        "name": "enc",
-        "videoQuality": "bad",
-        "transcript": "a transcript"
-      };
-      assert.equal(packager.getType(obj), 'VideoObject');
-    });
-
-    it('should add types to a document', function(){
-
-      var doc = {
-        "name": 'myname',
-        "author": { "givenName": "peter" },
-        "encoding": { "name": "enc" },
-        "hasPart": [
-          { "name": "a part" }
-        ]
-      };
-
-      var mdoc = packager.type(clone(doc), ['Article']);
-      assert.equal(mdoc['@type'], 'Article');
-      assert.equal(mdoc.author['@type'], 'Person');
-      assert.equal(mdoc.hasPart[0]['@type'], 'CreativeWork');
-    });
-
+  it('should infer type of a node', function(){
+    var obj = {
+      "schema:name": "a name",
+      "schema:videoQuality": "bad",
+      "schema:transcript": "a transcript"
+    };
+    assert.equal(schemaOrg.getType(obj, 'schema:CreativeWork'), 'schema:VideoObject');
   });
 
 });
